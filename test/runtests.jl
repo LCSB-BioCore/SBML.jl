667240f808f4feb5b818187b8a2df7b205d35f66
paulflang <paul.lang@wolfson.ox.ac.uk>
2021-06-01 14:58:53 +0100

    include all test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,9 @@ using Symbolics
 import Pkg
 
 @testset "SBML test suite" begin
-    # include("version.jl")
-    # include("ecoli_flux.jl")
-    # include("loadmodels.jl")
+    include("version.jl")
+    include("ecoli_flux.jl")
+    include("loadmodels.jl")
     include("sbml2symbolics.jl")
     include("reactionsystem.jl")
     include("loaddynamicmodels.jl")
