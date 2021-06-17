83d490fe0bfd42cc7861f362ea1ac4825e08ffa9
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2021-06-17 17:01:11 +0200

    make the mapping parametrizable by user
    This potentially saves a lot of headache with various custom functions or stuff
    where support may be complicated/ambiguous. In this precise case, we're
    allowing it because the user needs to `@register` the factorial function with
    Symbolics manually, so that it can be translated properly.

--- test/symbolics.jl
+++ test/symbolics.jl
@@ -6,18 +6,30 @@
     D = Symbolics.Variable(Symbol("D"))
     E = Symbolics.Variable(Symbol("E"))
 
-    test = SBML.MathApply("*", SBML.Math[
-        SBML.MathApply("+", SBML.Math[
-            SBML.MathApply("*", SBML.Math[
-                SBML.MathIdent("A"),
-                SBML.MathIdent("B")]),
-            SBML.MathApply("-", SBML.Math[
-                SBML.MathApply("*", SBML.Math[
-                    SBML.MathIdent("C"),
-                    SBML.MathIdent("D")])])]),           
-        SBML.MathIdent("E")])
+    test = SBML.MathApply(
+        "*",
+        SBML.Math[
+            SBML.MathApply(
+                "+",
+                SBML.Math[
+                    SBML.MathApply(
+                        "*",
+                        SBML.Math[SBML.MathIdent("A"), SBML.MathIdent("B")],
+                    ),
+                    SBML.MathApply(
+                        "-",
+                        SBML.Math[SBML.MathApply(
+                            "*",
+                            SBML.Math[SBML.MathIdent("C"), SBML.MathIdent("D")],
+                        )],
+                    ),
+                ],
+            ),
+            SBML.MathIdent("E"),
+        ],
+    )
 
-    expr_true = (A*B - C*D) * E
-    ex = convert(Num,test)
-    @test isequal(convert(Num,test), expr_true)
+    expr_true = (A * B - C * D) * E
+    ex = convert(Num, test)
+    @test isequal(convert(Num, test), expr_true)
 end
