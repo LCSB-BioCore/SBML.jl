cc1189170ea087e85df835e5bbe16c79efd205c0
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2021-06-17 17:10:16 +0200

    test ifelse

--- test/symbolics.jl
+++ test/symbolics.jl
@@ -29,7 +29,16 @@
         ],
     )
 
-    expr_true = (A * B - C * D) * E
-    ex = convert(Num, test)
-    @test isequal(convert(Num, test), expr_true)
+    @test isequal(convert(Num, test), (A * B - C * D) * E)
+
+    test = SBML.MathApply(
+        "piecewise",
+        SBML.Math[
+            SBML.MathApply("lt", SBML.Math[SBML.MathVal(1), SBML.MathVal(0)]),
+            SBML.MathVal(123),
+            SBML.MathVal(456),
+        ],
+    )
+
+    @test isequal(convert(Num, test), 456)
 end
