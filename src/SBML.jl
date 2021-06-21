8b267ac3f824f88ec91c4402c711d08f598c4ddd
Mosè Giordano <mose@gnu.org>
2021-06-21 17:52:55 +0100

    Make `sbml` a proper function, instead of a non-const anononymous one

--- src/SBML.jl
+++ src/SBML.jl
@@ -13,7 +13,7 @@ include("math.jl")
 include("utils.jl")
 include("symbolics.jl")
 
-sbml = (sym::Symbol) -> dlsym(SBML_jll.libsbml_handle, sym)
+sbml(sym::Symbol) = dlsym(SBML_jll.libsbml_handle, sym)
 
 export SBMLVersion, readSBML, getS, getLBs, getUBs, getOCs
 export set_level_and_version, libsbml_convert, convert_simplify_math
