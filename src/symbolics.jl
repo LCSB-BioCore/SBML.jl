b260cbf7685b4848bbc1a12a165fe4641aa9c0fb
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2022-03-25 18:14:57 +0100

    flip the type of the symbolics functions back to Any
    Mose explains:
    Function is abstract, so it isn't any better than Any, really, and not all
    "callable" objects in Julia are functions (think of type constructors or Python
    functions from PyCall, for example)

--- src/symbolics.jl
+++ src/symbolics.jl
@@ -22,7 +22,7 @@ sbmlRoot(x) = sqrt(x)
 sbmlRoot(power, x) = x^(1 / power)
 
 """
-    default_symbolics_mapping :: Dict{String,Function}
+    default_symbolics_mapping :: Dict{String,Any}
 
 Default mapping of SBML function names to Julia functions, represented as a
 dictionary from Strings (SBML names) to functions.
@@ -31,7 +31,7 @@ The default mapping only contains the basic SBML functions that are
 unambiguously represented in Julia; it is supposed to be extended by the user
 if more functions need to be supported.
 """
-const default_symbolics_mapping = Dict{String,Function}(
+const default_symbolics_mapping = Dict{String,Any}(
     "*" => *,
     "+" => +,
     "-" => -,
