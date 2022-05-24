ce76dd1573e70e9989c3789616239c90a8b8cbaf
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2022-05-24 18:00:17 +0200

    merge mireks changes

---
+++ src/interpret.jl
@@ -0,0 +1,140 @@
+
+# some helper functions for interpreting SBML math
+
+function sbmlPiecewise(args...)
+    if length(args) == 1
+        args[1]
+    elseif length(args) >= 3
+        IfElse.ifelse(args[2], args[1], sbmlPiecewise(args[3:end]...))
+    else
+        throw(DomainError(args, "malformed piecewise SBML function"))
+    end
+end
+
+sbmlNeq(a, b) = !isequal(a, b)
+
+sbmlLog(x) = sbmlLog(10, x)
+sbmlLog(base, x) = log(base, x)
+
+sbmlRoot(x) = sqrt(x)
+sbmlRoot(power, x) = x^(1 / power)
+
+sbmlRateOf(x) = throw(ErrorException("`rateOf' function mapping not defined"))
+
+"""
+    default_function_mapping :: Dict{String,Any}
+
+Default mapping of SBML function names to Julia functions, represented as a
+dictionary from Strings (SBML names) to functions.
+
+The default mapping only contains the basic SBML functions that are
+unambiguously represented in Julia; it is supposed to be extended by the user
+if more functions need to be supported.
+"""
+const default_function_mapping = Dict{String,Any}(
+    "*" => *,
+    "+" => +,
+    "-" => -,
+    "/" => /,
+    "abs" => abs,
+    "and" => &,
+    "arccos" => acos,
+    "arccosh" => acosh,
+    "arccot" => acot,
+    "arccoth" => acoth,
+    "arccsc" => acsc,
+    "arccsch" => acsch,
+    "arcsec" => asec,
+    "arcsech" => asech,
+    "arcsin" => asin,
+    "arcsinh" => asinh,
+    "arctan" => atan,
+    "arctanh" => atanh,
+    "ceiling" => ceil,
+    "cos" => cos,
+    "cosh" => cosh,
+    "cot" => cot,
+    "coth" => coth,
+    "csc" => csc,
+    "csch" => csch,
+    "eq" => isequal,
+    "exp" => exp,
+    "factorial" => factorial,
+    "floor" => floor,
+    "geq" => >=,
+    "gt" => >,
+    "leq" => <=,
+    "ln" => log,
+    "log" => sbmlLog,
+    "lt" => <,
+    "neq" => sbmlNeq,
+    "not" => !,
+    "or" => |,
+    "piecewise" => sbmlPiecewise,
+    "power" => ^,
+    "rateOf" => sbmlRateOf,
+    "root" => sbmlRoot,
+    "sech" => sech,
+    "sec" => sec,
+    "sinh" => sinh,
+    "sin" => sin,
+    "tanh" => tanh,
+    "tan" => tan,
+    "xor" => xor,
+)
+
+allowed_sym(x, allowed_funs) =
+    haskey(allowed_funs, x) ? allowed_funs[x] :
+    throw(DomainError(x, "Unknown SBML function"))
+
+"""
+    const default_constants::Dict{String, Any}
+
+A dictionary of default constants filled in place of SBML Math constants in the
+function conversion.
+"""
+const default_constants =
+    Dict{String,Any}("true" => true, "false" => false, "pi" => pi, "e" => exp(1))
+
+"""
+    function interpret_math(
+        x::SBML.Math;
+        map_apply = (fn::String, args, interpret::Function) -> SBML.default_function_mapping[fn](interpret.(args)...),
+        map_const = (x::SBML.MathConst) -> default_constants[x.id],
+        map_ident = (x::SBML.MathIdent) -> throw(ErrorException("identifier mapping not defined")),
+        map_lambda = (x::SBML.MathLambda) -> throw(ErrorException("lambda function mapping not defined")),
+        map_time = (x::SBML.MathTime) -> throw(ErrorException("time mapping not defined")),
+        map_value = (x::SBML.MathVal) -> x.val,
+    )
+
+Recursively interpret SBML.[`Math`](@ref) type. This can be used to relatively
+easily traverse and evaluate the SBML math, or translate it into any custom
+representation, such as `Expr` or the `Num` of Symbolics.jl (see the SBML test
+suite for examples).
+
+By default, the function can convert SBML constants, values and function
+applications, but identifiers, time values and lambdas are not mapped and throw
+an error. Similarly SBML function `rateOf` is undefined, users must to supply
+their own definition of `rateOf` that uses the correct derivative.
+"""
+function interpret_math(
+    x::SBML.Math;
+    map_apply = (x::SBML.MathApply, interpret::Function) ->
+        SBML.default_function_mapping[x.fn](interpret.(x.args)...),
+    map_const = (x::SBML.MathConst) -> default_constants[x.id],
+    map_ident = (x::SBML.MathIdent) ->
+        throw(ErrorException("identifier mapping not defined")),
+    map_lambda = (x::SBML.MathLambda, interpret::Function) ->
+        throw(ErrorException("lambda function mapping not defined")),
+    map_time = (x::SBML.MathTime) -> throw(ErrorException("time mapping not defined")),
+    map_value = (x::SBML.MathVal) -> x.val,
+)
+    interpret(x::SBML.MathApply) = map_apply(x, interpret)
+    interpret(x::SBML.MathConst) = map_const(x)
+    interpret(x::SBML.MathIdent) = map_ident(x)
+    interpret(x::SBML.MathLambda) = map_lambda(x, interpret)
+    interpret(x::SBML.MathTime) = map_time(x)
+    interpret(x::SBML.MathVal) = map_value(x)
+
+    interpret(x)
+end
--- test/symbolics.jl
+++ test/symbolics.jl
@@ -1,5 +1,39 @@
 
-@testset "Math to Symbolics conversions" begin
+# Conversion to symbolics
+symbolicsRateOf(x) = Symbolics.Differential(convert(Num, MathTime("t")))(x)
+
+symbolics_mapping = Dict(SBML.default_function_mapping..., "rateOf" => symbolicsRateOf)
+
+map_symbolics_time_ident(x) = begin
+    sym = Symbol(x.id)
+    Symbolics.unwrap(first(@variables $sym))
+end
+
+const interpret_as_num(x::SBML.Math) = SBML.interpret_math(
+    x;
+    map_apply = (x::SBML.MathApply, interpret::Function) ->
+        Num(symbolics_mapping[x.fn](interpret.(x.args)...)),
+    map_const = (x::SBML.MathConst) -> Num(SBML.default_constants[x.id]),
+    map_ident = map_symbolics_time_ident,
+    map_lambda = (_, _) ->
+        throw(ErrorException("Symbolics.jl does not support lambda functions")),
+    map_time = map_symbolics_time_ident,
+    map_value = (x::SBML.MathVal) -> Num(x.val),
+)
+
+@testset "Symbolics compatibility" begin
+
+    test = SBML.MathApply(
+        "piecewise",
+        SBML.Math[
+            SBML.MathVal(123),
+            SBML.MathApply("lt", SBML.Math[SBML.MathVal(1), SBML.MathVal(0)]),
+            SBML.MathVal(456),
+        ],
+    )
+
+    @test isequal(interpret_as_num(test), 456)
+
     @variables A B C D Time
 
     test = SBML.MathApply(
