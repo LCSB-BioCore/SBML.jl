3e38a58dabd352c4b4c6228fe123993d07efd128
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2026-05-02 09:16:33 +0200

    fix tests for symbolics 7

--- test/loadmodels.jl
+++ test/loadmodels.jl
@@ -345,7 +345,11 @@ end
     m = readSBML(joinpath(@__DIR__, "data", "01565-sbml-l3v1.xml"))
 
     if TEST_SYMBOLICS
-        @test interpret_as_num(m.reactions["J23"].kinetic_math) == 0.0
+        @variables S23 S23b
+        @test isequal(
+            interpret_as_num(m.reactions["J23"].kinetic_math),
+            (S23*S23b*log(1)) / log(10),
+        )
 
         @variables S29 S29b
         @test isequal(interpret_as_num(m.reactions["J29"].kinetic_math), 2.0 * S29 * S29b)
